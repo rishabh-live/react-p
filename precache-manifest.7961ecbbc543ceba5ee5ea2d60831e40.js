f8f51a2ae30c290bc1f6f8faaf82dfa15c163971
rishabharya1422 <rishabharya1422@users.noreply.github.com>
2020-08-07 14:34:15 +0530

    Updates

---
+++ precache-manifest.7961ecbbc543ceba5ee5ea2d60831e40.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ca65df921c568985bd98ea774da7cb91",
+    "url": "/react-p/index.html"
+  },
+  {
+    "revision": "210f7c8f8c1e3a0f816f",
+    "url": "/react-p/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "c3deb36ea35ce28b0578",
+    "url": "/react-p/static/js/2.efea42fd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-p/static/js/2.efea42fd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "210f7c8f8c1e3a0f816f",
+    "url": "/react-p/static/js/main.c30d675a.chunk.js"
+  },
+  {
+    "revision": "a115b7f967fc52d0c98b",
+    "url": "/react-p/static/js/runtime-main.06b97ec9.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-p/static/media/logo.5d5d9eef.svg"
+  }
+]);
